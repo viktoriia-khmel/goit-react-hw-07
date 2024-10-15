1ba7d37a1dd382884187bdd98c8065c1fbbcc4e1
Viktoriia Khmel <legchilova.viktoria@gmail.com>
2024-10-15 15:09:17 +0300

    vercel.json and delete persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// import { useState, useEffect } from 'react'
-// import initialContacts from './assets/contacts.json'
+
 import './App.css'
 import ContactForm from './components/ContactForm/ContactForm'
 import SearchBox from './components/SearchBox/SearchBox';
@@ -7,28 +6,6 @@ import ContactList from './components/ContactList/ContactList';
 
 const App = () => {
 
-  // const [contacts, setContacts] = useState(() => {
-  //   const contactList = JSON.parse(window.localStorage.getItem("contacts"));
-  //   return contactList?.length ? contactList : initialContacts
-  // });
-
-  // const [filter, setFilter] = useState("");
-
-  //  useEffect(() => {
-  //   window.localStorage.setItem("contacts", JSON.stringify(contacts));
-  // }, [contacts]);
-  
-//   const handleAdding = (newContact) => {
-//     setContacts((prev) => [...prev, newContact])
-    
-//   };
-//   const deleteContact = (contactId) => {
-//     setContacts(prevContacts => {
-//       return prevContacts.filter((contact) => contact.id !== contactId);
-//   })
-// }
-
-//   const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
   
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store, persistor } from './redux/store.js'
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from './redux/store.js'
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}> {}
+    
       <App />
-       </PersistGate>
+       
   </Provider>,
 )
